7d04a29d6abcc0a95b282902c68a44bcaf09aa92
Jason Fang <mewtwo_jason@hotmail.com>
2013-04-04 09:44:43 -0400

    pimped up level screen

--- js/level-selection.js
+++ js/level-selection.js
@@ -1,20 +1,28 @@
 var EASY = 1; 
 var MEDIUM = 2; 
 var HARD = 3;
+var done = [1];
+var available = [];
 
 function setDifficulty(buttonNumber){
-    // 1-3 YK,NWT,BC 4-6 NVT,ALB,SAS 7-8 MAN, ON
+    // EASY: 1-3 YK,NWT,BC 
+    // MEDIUM: 4-9 NVT,ALB,SAS,NB,PEI,NS
+    // HARD: 10-13 QC, NFL, MAN, ON
     if(buttonNumber<=3){
         sessionStorage.difficulty = 1;
     }
     else{
-        if(buttonNumber <= 6){
+        if(buttonNumber <= 9){
             sessionStorage.difficulty = 2;
         }
         else{
             sessionStorage.difficulty = 3;
         }
     }
+}
+
+function updateGame(finishedPlaces, availablePlaces){
+    
 }
 
 $(document).ready(function() {
