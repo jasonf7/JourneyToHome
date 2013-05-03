c49eb9cf94c06718bc585d44a1c7c9ae30d4a31e
Jason Fang <mewtwo_jason@hotmail.com>
2013-05-03 09:36:35 -0400

    added tsx files

---
+++ data/land-tiles-simple.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="land-tiles-simple" tilewidth="40" tileheight="40">
+ <image source="land-tiles-simple.png" width="240" height="240"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+</tileset>
