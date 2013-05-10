a521cdcbbf1f4388b0c1866e54bd3c3f39c496b3
Aly <aly.hassan9@gmail.com>
2013-05-09 23:47:41 -0400

    added water tileset

---
+++ data/land-tiles-simple-water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="land-tiles-client" tilewidth="40" tileheight="40">
+ <image source="land-tiles-client.png" width="160" height="280"/>
+</tileset>
